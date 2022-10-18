ccb9a9953fca48e772b1276bcc664602b1b61d28
Rafael Silva <rafael.silva@instaletecnologia.com.br>
2022-10-18 10:30:50 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,104 @@
+import express from 'express'
+import cors from 'cors'
+import { PrismaClient } from '@prisma/client'
+import { PrismaClientRustPanicError } from '@prisma/client/runtime'
+import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes'
+import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-string'
+
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors());
+
+const prisma = new PrismaClient({
+  log: ['query']
+})
+
+app.get('/games', async (request, response) => {
+const games = await prisma.game.findMany({include:{
+  _count:{
+    select: {
+      Ads: true,
+    }
+  }
+}})
+
+  return response.json(games);
+})
+
+app.post('/games/:id/ads', async (request, response) => {
+
+  const gameId = request.params.id;
+  const body:any = request.body;
+  
+  const ad = await prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+		  yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+		  weekDays:	body.weekDays.join(','),
+		  hourStart: convertHourStringToMinutes(body.hourStart),
+		  hourEnd: convertHourStringToMinutes(body.hourEnd),
+		  useVoiceChannel: body.useVoiceChannel,
+    }
+  })
+
+  return response.status(201).json(ad);
+})
+
+app.get('/games/:id/ads', async (request, response) => {
+
+  const gameId = request.params.id;
+
+  const ads = await prisma.ad.findMany({
+    select:{
+      id: true,
+      name: true,
+      yearsPlaying: true,
+      weekDays: true,
+      hourStart: true,
+      hourEnd: true,
+      useVoiceChannel: true,
+     },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+    
+  })
+  // const gameID = request.params.id ;
+  return response.json(ads.map(ad => {
+    return {
+      ...ad,
+      weekDays: ad.weekDays.split(','),
+      hourStart: convertMinutesToHourString(ad.hourStart),
+      hourEnd: convertMinutesToHourString(ad.hourEnd)
+    }
+   
+  }))
+})
+
+
+app.get('/ads/:id/discord', async (request, response) => {
+  const adId = request.params.id;
+
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord:true,
+    },
+    where: {
+      id: adId,
+    }
+
+  })
+
+  return response.json({discord: ad.discord,})
+})
+
+
+app.listen(3333)
---
+++ web/src/components/Form/Input.tsx
@@ -0,0 +1,11 @@
+import {ImgHTMLAttributes} from 'react'
+
+interface InputProps extends ImgHTMLAttributes<HTMLInputElement>{}
+
+export function Input(props: InputProps) {
+  return (
+    <input 
+    {...props}       
+        className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500"/>
+  )
+}
